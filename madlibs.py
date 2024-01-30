df6bd21555d8d9aaf8ff4b73803d3fd6e6ec9f9c
xchristina04 <vy.xchristina@gmail.com>
2024-01-29 23:05:43 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,18 @@
 # TODO Create your inputs, variables, and add your story here!
+
+noun = input("Enter a noun: ")
+adj = input("Enter an adjective: ")
+verb = input("Enter a present tense verb: ")
+
+pronoun = input("Enter a pronoun: ")
+name = input("Enter a name: ")
+color = input("Enter a color: ")
+number = input("Enter a number: ")
+animal = input("Enter an animal: ")
+holiday = input("Enter a holiday: ")
+place = input("Enter a place: ")
+number2 = input("Enter another number: ")
+adj2 = input("Enter another adjective: ")
+food = input("Enter a food: ")
+
+print(f"During {holiday}, {pronoun} was with {name} to find {number} {adj} {noun}s that were going to {verb}. {name} was hoping the {noun}s could help track their {animal}. After {number2} days, {noun} finally found their {adj2} {animal} at {place} munching on {food}.")
